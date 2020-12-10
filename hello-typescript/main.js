c1fdec8c9c9672e64fa276dd86c030484cdbfb1b
ANAÍLSON RIBEIRO <arsantos315@gmail.com>
2020-12-10 16:49:04 -0300

    EstudoAngularJs

---
+++ hello-typescript/main.js
@@ -0,0 +1,12 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+//ES 6 OU ES 2015
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 4];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); //ES 2015
---
+++ hello-typescript/main.ts
@@ -0,0 +1,18 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x,y){
+    return x + y;
+}
+
+//ES 6 OU ES 2015
+let num = 2;
+const PI = 3.14;
+
+
+var numeros = [1,2,4];
+numeros.map(function(valor){
+
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2); //ES 2015
